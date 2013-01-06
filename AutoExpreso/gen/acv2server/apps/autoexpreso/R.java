e9b5cb3e094cd8a9018aa18a96bf27eaf3972c23
Jose Acevedo <joseacevedo@Joses-MacBook-Pro.local>
2013-01-05 23:37:33 -0400

    added icon

--- AutoExpreso/gen/acv2server/apps/autoexpreso/R.java
+++ AutoExpreso/gen/acv2server/apps/autoexpreso/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int autoexpreso=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int menu_settings=0x7f070001;
@@ -25,6 +26,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int exit=0x7f040005;
         public static final int menu_settings=0x7f040004;
         public static final int title_section1=0x7f040003;
         public static final int title_section2=0x7f040002;
