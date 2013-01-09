5823c2baaad7b57588ea08f5224314dc8b9830b3
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-01-09 17:59:55 -0400

    Added devogella code for HttpURLConnection

--- AutoExpreso/src/acv2server/apps/autoexpreso/fragments/MainWindow.java
+++ AutoExpreso/src/acv2server/apps/autoexpreso/fragments/MainWindow.java
@@ -1,8 +1,10 @@
 package acv2server.apps.autoexpreso.fragments;
 
 import acv2server.apps.autoexpreso.R;
+import acv2server.apps.autoexpreso.http.HttpTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +30,8 @@ public class MainWindow extends Fragment {
 			
 			@Override
 			public void onClick(View v) {
-				
+				Log.d("DEBUG", "Clicked");
+				new HttpTask().execute("https://m.autoexpreso.com/Login/Login","joseacevedo11","Quemierda123");
 			}
 		});
 		
---
+++ AutoExpreso/src/acv2server/apps/autoexpreso/http/HttpTask.java
@@ -0,0 +1,68 @@
+package acv2server.apps.autoexpreso.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class HttpTask extends AsyncTask<String, Integer, String> {
+
+	
+	@Override
+	protected String doInBackground(String... params) {
+		// TODO Load the webpage with the credentials given by the user
+		// params is an array that contains the url of AutoExpreso in [0] , username in [1] and password in [2] 
+		
+		String urlS = params[0];
+		String username = params[1];
+		String password = params[2];
+
+		try{
+			URL url = new URL(urlS);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			readStream(connection.getInputStream());
+		}
+		catch(Exception e)
+		{
+			Log.d("DEBUG", "Exploto");
+		}
+		
+		return null;
+	}
+
+	
+	@Override
+	protected void onPostExecute(String result) {
+		//TODO Update the UI 
+		super.onPostExecute(result);
+	}
+	
+	
+	private void readStream(InputStream in) {
+		  BufferedReader reader = null;
+		  try {
+		    reader = new BufferedReader(new InputStreamReader(in));
+		    String line = "";
+		    while ((line = reader.readLine()) != null) {
+		    	Log.d("DEBUG", line);
+		    }
+		  } catch (IOException e) {
+		    e.printStackTrace();
+		  } finally {
+		    if (reader != null) {
+		      try {
+		        reader.close();
+		      } catch (IOException e) {
+		        e.printStackTrace();
+		        }
+		    }
+		  }
+		} 
+	
+
+}
