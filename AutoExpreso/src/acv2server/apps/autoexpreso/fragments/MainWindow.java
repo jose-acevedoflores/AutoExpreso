339e439f1c77535611022d0098b1a41ef506f81e
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-01-17 12:42:03 -0400

    Added Keep me logged in option

--- AutoExpreso/src/acv2server/apps/autoexpreso/fragments/MainWindow.java
+++ AutoExpreso/src/acv2server/apps/autoexpreso/fragments/MainWindow.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -24,12 +25,17 @@ import android.widget.Toast;
  */
 public class MainWindow extends Fragment {
 
+	private EditText usernameET;
+	private EditText passwordET;
+
 	public View onCreateView(LayoutInflater inflater, final ViewGroup container , Bundle savedInstanceState)
 	{	
 		//Ojo: attachToRoot tiene que ser false si no tira un error ?????
 		View root =inflater.inflate(R.layout.mod_online_view, container, false);
 
 		Button b = (Button) root.findViewById(R.id.bLogin);
+		usernameET = (EditText) root.findViewById(R.id.etUsername);
+		passwordET = (EditText) root.findViewById(R.id.etPassword);
 
 		b.setOnClickListener(new View.OnClickListener() {
 
@@ -37,8 +43,31 @@ public class MainWindow extends Fragment {
 			public void onClick(View v) {
 				Log.d("DEBUG", "Clicked");
 				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(container.getContext());
-				String username = prefs.getString("username_preference", "");
-				String password = prefs.getString("password_preference", "");
+
+				boolean keepMeLogged = prefs.getBoolean("keep_user_logged", false);
+				String username;
+				String password;
+				//If the user wants to stay logged check settings values for username and password.
+				if(keepMeLogged)
+				{
+					username = prefs.getString("username_preference", "");
+					password = prefs.getString("password_preference", "");
+					//If the user checked the keepmelooged in flag but didn't provided values for the username and password
+					//fields try getting values from the EditText Views. 
+					if(username.equals("") || password.equals(""))
+					{
+						Toast t = Toast.makeText(container.getContext(), "No username or password found in settings", Toast.LENGTH_SHORT);
+						t.show();
+						username = usernameET.getText().toString();
+						password = passwordET.getText().toString(); 
+					}
+				}
+				//If the user did not checked the keepmelooged in flag fields try getting values from the EditText Views. 
+				else
+				{
+					username = usernameET.getText().toString();
+					password = passwordET.getText().toString(); 
+				}
 				String url = prefs.getString("website_url", "https://m.autoexpreso.com/Login/Login");
 
 				if(username.equals("") || password.equals(""))
@@ -61,7 +90,7 @@ public class MainWindow extends Fragment {
 
 		return root;
 	}
-	
+
 	/**
 	 * 
 	 * @param context the context of this ??? 
